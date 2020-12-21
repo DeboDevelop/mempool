e13a3000c1841c844556c12f61b7b70ffd1385f2
DeboDevelop <debajyotidofficial@gmail.com>
2020-12-21 15:32:03 +0530

    Added Tailwind CSS and styled the navbar.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 import AllCards from "./Components/AllCards";
 import Nav from "./Components/Nav";
 import firebase, { auth } from "./firebase";
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -61,18 +61,45 @@ function Nav({ user, setUser, setCardState }) {
             .catch(err => console.log(err));
     };
     return (
-        <nav>
+        <nav className="bg-blue-300 h-auto border-b-2">
             {user.id ? (
-                <div>
-                    <form onSubmit={handleSubmit}>
-                        <input type="text" placeholder="Front" value={state.front} onChange={handleFront} />
-                        <input type="text" placeholder="Back" value={state.back} onChange={handleBack} />
-                        <button>Add Card</button>
-                    </form>
-                    <button onClick={logout}>Logout</button>{" "}
+                <div className="md:flex md:justify-between">
+                    <div className="container">
+                        <form onSubmit={handleSubmit} className="md:flex md:justify-start">
+                            <input
+                                type="text"
+                                placeholder="Front"
+                                className="border w-72 py-2 px-3 m-2 text-grey-darkest border-gray-600 rounded"
+                                value={state.front}
+                                onChange={handleFront}
+                            />
+                            <input
+                                type="text"
+                                placeholder="Back"
+                                className="border w-72 py-2 px-3 m-2 text-grey-darkest border-gray-600 rounded"
+                                value={state.back}
+                                onChange={handleBack}
+                            />
+                            <div>
+                                <button className="bg-yellow-300 hover:bg-yellow-400 text-lg px-2 py-1 m-2 rounded-lg">
+                                    Add Card
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <button
+                        onClick={logout}
+                        className="bg-pink-400 hover:bg-pink-600 text-lg px-2 py-1 m-2 rounded-lg flex-grow-0">
+                        Logout
+                    </button>{" "}
                 </div>
             ) : (
-                <button onClick={login}>Login</button>
+                <div className="sm:flex justify-between">
+                    <h1 className="my-auto text-lg mx-2">Welcome to Mempool</h1>
+                    <button onClick={login} className="bg-pink-400 hover:bg-pink-600 text-lg px-2 py-1 m-2 rounded-lg">
+                        Login
+                    </button>
+                </div>
             )}
         </nav>
     );
